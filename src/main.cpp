46b498e48c4772bd0d5121b2d9122ae1b585ed0e
Ceejay <cjmarineson@gmail.com>
2025-05-11 22:08:14 -0400

    added socket, bind, listen functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <unistd.h>
+#include <string>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main()
 {
-    std::cout << "WSL is working!\n";
+    char buffer[30000] = {0};
+
+    std::string response = 
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 13\r\n"
+        "\r\n"
+        "Hello World!\r\n";
+
+    int server = socket(AF_INET, SOCK_STREAM, 0);
+    if (server < 1)
+    {
+        perror("socket() failed.");
+        return 1;
+    }
+
+    // "...store addresses for the Internet address family. 
+    // Values of this type shall be cast by applications to struct sockaddr for use with socket functions." - opengroup.org
+    struct sockaddr_in address; // created on stack
+    constexpr int port{8080};
+
+    socklen_t addrlen = sizeof(address);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+    if (bind(server, (struct sockaddr*)&address, addrlen) < 0)
+    {
+        perror("bind() failed");
+        return 1;
+    }
+
+    if (listen(server, 3) < 0)
+    {
+        perror("listen() failed");
+        return 1;
+    }
+
     return 0;
 }
