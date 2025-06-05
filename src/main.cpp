176e02a4b43dc4eaf9587fe371f66c79ff0cf955
Ceejay <cjmarineson@gmail.com>
2025-06-05 15:58:20 -0400

    Fixed minor linker issue with config.hpp between main.cpp and request_handler.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,17 @@
 #include "inet_addr.hpp"
 #include "http_connection_socket.hpp"
 #include "connection_handler.hpp"
-#include "config.hpp"
+#include <thread>
+
+static constexpr int server_port = 8081;
 
 int main()
 {
     HTTPServerSocket server;
-    INetAddr server_addr{Config::SERVER_PORT, "127.0.0.1"};
+    INetAddr server_addr{server_port, "127.0.0.1"};
     server.bind_server(server_addr);
     server.listen_server();
+    std::cout << "Number of concurrent threads: " << std::thread::hardware_concurrency() << '\n';
     
     while (true) {
         // A connection is it's own independent, executing entity
