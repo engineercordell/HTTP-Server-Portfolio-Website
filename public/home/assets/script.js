118d26c8fa9ee79295d20e11583fdd3d7ec33a35
Ceejay <cjmarineson@gmail.com>
2025-07-12 23:22:11 -0400

    Adjusted and established page layout pattern

--- public/home/assets/script.js
+++ public/home/assets/script.js
@@ -1,11 +1,41 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    await sleep(1500);
-    await typeText("Visitor detected", 40, "terminal-text");
     await sleep(1000);
-    await typeText("...", 300, "terminal-text");
+    await typeText("Visitor detected", 40, "terminal-command-text");
     await sleep(1000);
-    await typeText(" executing", 40, "terminal-text");
-    await typeText("\u00A0greetings.exe", 40, "terminal-text-program");
+    await typeText("...", 300, "terminal-command-text");
+    await sleep(1000);
+    await typeText("executing", 40, "terminal-command-text");
+    await typeText("\u00A0greetings.exe", 40, "terminal-command-program");
+    await typeText("...", 40, "terminal-command-ellipsis");
+    await sleep(500);
+    document.querySelector('.cursor').classList.add('stopped');
+    await sleep(500);
+
+
+    const terminal = document.querySelector('.terminal-landing');
+    let oldCursor = document.querySelector('.cursor');
+    oldCursor.remove();
+    const descLine = document.createElement('div');
+    descLine.classList.add('terminal-desc');
+    descLine.innerHTML = `
+        <span class="text-wrapper">
+            <span id="terminal-desc-text"></span><span class="cursor">_</span>
+        </span>
+    `;
+    terminal.appendChild(descLine);
+    await typeText("Hello there, I'm Cordell. A multifaceted engineer and Georgia Tech graduate.", 20, "terminal-desc-text");
+    oldCursor = document.querySelector('.cursor');
+    oldCursor.remove();
+
+    const aboutLine = document.createElement('div');
+    aboutLine.classList.add('terminal-more');
+    aboutLine.innerHTML = `
+        <span class="text-wrapper">
+            <span id="terminal-more-text"></span><span class="cursor">_</span>
+        </span>
+    `;
+    terminal.appendChild(aboutLine);
+    await typeText("Click 'Resume' above to download my resume, or keep scrolling to learn more about me!", 20, "terminal-more-text");
 });
 
 function sleep(ms) {
