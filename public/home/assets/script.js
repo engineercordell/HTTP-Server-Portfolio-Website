adc259af18fee4e64cbd1efdc6422e624bef1019
Ceejay <cjmarineson@gmail.com>
2025-07-16 23:36:58 -0400

    Added cursor effect to 'about-me' section

--- public/home/assets/script.js
+++ public/home/assets/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    await sleep(1000);
+    await sleep(750);
     await typeText("Visitor detected", 40, "terminal-command-text");
     await sleep(1000);
     await typeText("...", 300, "terminal-command-text");
@@ -11,9 +11,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     document.querySelector('.cursor').classList.add('stopped');
     await sleep(500);
 
-
     const terminal = document.querySelector('.terminal-text');
-    let oldCursor = document.querySelector('.cursor');
+    let oldCursor = document.querySelector('.terminal-command .cursor');
     oldCursor.remove();
     const descLine = document.createElement('div');
     descLine.classList.add('terminal-desc');
@@ -24,7 +23,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     `;
     terminal.appendChild(descLine);
     await typeText("Hello there, I'm Cordell. A multifaceted Georgia Tech engineer.", 20, "terminal-desc-text");
-    oldCursor = document.querySelector('.cursor');
+    oldCursor = document.querySelector('.terminal-desc .cursor');
     oldCursor.remove();
 
     const aboutLine = document.createElement('div');
@@ -36,13 +35,59 @@ document.addEventListener('DOMContentLoaded', async () => {
     `;
     terminal.appendChild(aboutLine);
     await typeText("Click 'Resume' above to download my resume, or keep scrolling to learn more about me!", 20, "terminal-more-text");
+
+    oldCursor = document.querySelector('.terminal-more .cursor');
+    oldCursor.remove();
 });
+
+// about-me listener
+document.addEventListener('DOMContentLoaded', () => {
+  const observer = new IntersectionObserver(handleAboutMeReveal, {
+    threshold: 0.1
+  });
+
+  const aboutSection = document.getElementById('about-me');
+  observer.observe(aboutSection);
+});
+
+let aboutHasAnimated = false;
+
+async function handleAboutMeReveal(entries) {
+  const entry = entries[0];
+  if (entry.isIntersecting && !aboutHasAnimated) {
+    aboutHasAnimated = true;
+
+    await typeTextAdvanced("./about-me", 50, "about-command", true);
+    await sleep(200);
+
+    await typeTextAdvanced("Background", 5, "about-back-head", true);
+
+    await typeTextAdvanced(`
+        Hey there—My name is Cordell Palmer. In December 2024, I graduated from Georgia Tech 
+        with a Bachelor of Science in Mechanical Engineering and a Minor in Computer Science, with a focus on Machine Learning.
+        During my undergraduate years, I was able to cultivate a broad set of skills, having obtained proficiency in areas such as
+        CAD modeling/3D printing, structural analysis, software and hardware programming, AI, robotics, and wide range of electrical and mechanical
+        data analysis/manufacturing tools. Despite my background being primarily rooted in mechanical systems, my true passion lies at the intersection of hardware, 
+        software, and AI development.`, 5, "about-line-1", true);
+
+    await typeTextAdvanced(`
+        Since graduation I've deeply immersed myself in systems development, having written the HTTP server that
+        runs this website from scratch in C++ with barebones kernel APIs. The journey to enrich my understanding of networking, memory, and 
+        OS-level architecture has sparked an ever evolving passion in all things embedded systems, FPGAs, digital circuitry, and future-forward
+        technologies like graphene semiconductors.
+        `, 5, "about-line-2", true);
+
+    await typeTextAdvanced("In short: I enjoy building things that closely interact with our world, and this is only the beginning.", 5, "about-line-3", true);
+  }
+}
+
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function typeText(text, charDelay, elemID) {
+async function typeText(text, charDelay, elemID) {
+
     return new Promise((resolve) => {
         const target = document.getElementById(elemID);
         let index = 0;
@@ -59,4 +104,22 @@ function typeText(text, charDelay, elemID) {
 
         typeChar();
     });
+}
+
+async function typeTextAdvanced(text, delay, targetClass, removeCursorOnEnd = false) {
+    const targetEl = document.getElementById(targetClass);
+
+    const cursorSpan = document.createElement("span");
+    cursorSpan.classList.add("cursor");
+    cursorSpan.textContent = "_";
+    targetEl.appendChild(cursorSpan);
+
+    for (let i = 0; i < text.length; i++) {
+        cursorSpan.insertAdjacentText("beforebegin", text[i]);
+        await sleep(delay);
+    }
+
+    if (removeCursorOnEnd) {
+        cursorSpan.remove();
+    }
 }
