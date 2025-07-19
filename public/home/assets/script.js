20fc7b319a9a8a8e15fd38d4b01c1f0014fbdd4b
Ceejay <cjmarineson@gmail.com>
2025-07-19 00:32:43 -0400

    Fixed duplicate text issue when clicking divs to skip text

--- public/home/assets/script.js
+++ public/home/assets/script.js
@@ -1,3 +1,8 @@
+let terminalCancelToken = { canceled: false };
+let aboutCancelToken = { canceled: false };
+let aboutHasAnimated = false;
+
+// when page is loaded, 'terminal' animation plays
 document.addEventListener('DOMContentLoaded', async () => {
     await sleep(750);
     await typeText("Visitor detected", 40, "terminal-command-text", "terminal-cursor", terminalCancelToken);
@@ -9,7 +14,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     await typeText("...", 40, "terminal-command-ellipsis", "terminal-cursor", terminalCancelToken);
     await sleep(1000);
 
-    const terminal = document.querySelector('.terminal-text');
+    const terminal = document.getElementById('terminal-text');
 
     const descLine = document.createElement('div');
     descLine.classList.add('terminal-desc');
@@ -32,21 +37,29 @@ document.addEventListener('DOMContentLoaded', async () => {
     await typeText("Click 'Resume' above to download my resume, or keep scrolling to learn more about me!", 20, "terminal-more-text", "terminal-cursor", terminalCancelToken);
 });
 
-// about-me listener
+// when page is loaded, observe when 'about-me' scrolls into view
 document.addEventListener('DOMContentLoaded', () => {
-  const observer = new IntersectionObserver(handleAboutMeReveal, {
-    threshold: 0.1
-  });
+  const observer = new IntersectionObserver(handleAboutMeReveal, { threshold: 0.3 });
 
   const aboutSection = document.getElementById('about-me');
   observer.observe(aboutSection);
 });
 
-// let typingCanceled = false;
-const terminalCancelToken = { canceled: false };
-const aboutCancelToken = { canceled: false };
+document.getElementById('terminal-panel').addEventListener("click", async () => {
+    terminalCancelToken.canceled = true;
 
-let aboutHasAnimated = false;
+    const lastTerminalCursor = document.getElementById("terminal-cursor");
+    if (lastTerminalCursor) lastTerminalCursor.remove();
+});
+
+// 'about-background' is clicked
+document.getElementById("about-background").addEventListener("click", async () => {
+    aboutCancelToken.canceled = true;
+
+    const lastAboutCursor = document.getElementById("about-cursor");
+    if (lastAboutCursor) lastAboutCursor.remove();
+});
+
 async function handleAboutMeReveal(entries) {
   const entry = entries[0];
   if (entry.isIntersecting && !aboutHasAnimated) {
@@ -56,7 +69,6 @@ async function handleAboutMeReveal(entries) {
     await sleep(200);
 
     await typeText("Background", 1, "about-back-head", "about-cursor", aboutCancelToken);
-
     await typeText(`
         Hey there—My name is Cordell Palmer. In December 2024, I graduated from Georgia Tech 
         with a Bachelor of Science in Mechanical Engineering and a Minor in Computer Science, with a focus on Machine Learning.
@@ -64,40 +76,16 @@ async function handleAboutMeReveal(entries) {
         CAD modeling/3D printing, structural analysis, software and hardware programming, AI, robotics, and wide range of electrical and mechanical
         data analysis/manufacturing tools. Despite my background being primarily rooted in mechanical systems, my true passion lies at the intersection of hardware, 
         software, and AI development.`, 1, "about-line-1", "about-cursor", aboutCancelToken);
-
     await typeText(`
         Since graduation I've deeply immersed myself in systems development, having written the HTTP server that
         runs this website from scratch in C++ with barebones kernel APIs. The journey to enrich my understanding of networking, memory, and 
         OS-level architecture has sparked an ever evolving passion in all things embedded systems, FPGAs, digital circuitry, and future-forward
-        technologies like graphene semiconductors.
-        `, 1, "about-line-2", "about-cursor", aboutCancelToken);
-
+        technologies like graphene semiconductors.`, 1, "about-line-2", "about-cursor", aboutCancelToken);
     await typeText("In short: I enjoy building things that closely interact with our world, and this is only the beginning.", 1, "about-line-3", "about-cursor", aboutCancelToken);
   }
-
-
 }
 
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-document.getElementById("about-background").addEventListener("click", () => {
-    aboutCancelToken.canceled = true;
-
-    const aboutLine1 = document.getElementById("about-line-1");
-    aboutLine1.textContent = sections["about-section"][2].text;
-    const aboutLine2 = document.getElementById("about-line-2");
-    aboutLine2.textContent = sections["about-section"][3].text;
-    const aboutLine3 = document.getElementById("about-line-3");
-    aboutLine3.textContent = sections["about-section"][4].text;
-
-    const lastCursor = document.getElementById("about-cursor");
-    if (lastCursor) lastCursor.remove();
-});
-
-async function typeText(text, delay, targetID, cursorID, removeCursorOnEnd = true, cancelToken) {
+async function typeText(text, delay, targetID, cursorID, cancelToken, removeCursorOnEnd = true) {
     const targetEl = document.getElementById(targetID);
 
     const oldCursor = document.getElementById(cursorID);
@@ -109,7 +97,7 @@ async function typeText(text, delay, targetID, cursorID, removeCursorOnEnd = tru
     targetEl.appendChild(cursorSpan);
 
     for (let i = 0; i < text.length; i++) {
-        if (cancelToken?.canceled) {
+        if (cancelToken.canceled) {
             targetEl.insertAdjacentText("beforeend", text.slice(i));
             break;
         }
@@ -123,35 +111,38 @@ async function typeText(text, delay, targetID, cursorID, removeCursorOnEnd = tru
 }
 
 // each 'section/content-div' will have it's own typeText() to dynamically write to it
-const sections = {
-    "about-section": [
-        { text: "./about-me", delay: 50, targetID: "about-command", removeCursor: true},
-        { text: "Background", delay: 1, targetID: "about-back-head", removeCursor: true},
-        { text: `Hey there—My name is Cordell Palmer. In December 2024, I graduated from Georgia Tech 
-            with a Bachelor of Science in Mechanical Engineering and a Minor in Computer Science, with a focus on Machine Learning.
-            During my undergraduate years, I was able to cultivate a broad set of skills, having obtained proficiency in areas such as
-            CAD modeling/3D printing, structural analysis, software and hardware programming, AI, robotics, and wide range of electrical and mechanical
-            data analysis/manufacturing tools. Despite my background being primarily rooted in mechanical systems, my true passion lies at the intersection of hardware, 
-            software, and AI development.`, 
-        delay: 1, 
-        targetID: "about-line-1", 
-        removeCursor: true},
-        { text: `Since graduation I've deeply immersed myself in systems development, having written the HTTP server that
-            runs this website from scratch in C++ with barebones kernel APIs. The journey to enrich my understanding of networking, memory, and 
-            OS-level architecture has sparked an ever evolving passion in all things embedded systems, FPGAs, digital circuitry, and future-forward
-            technologies like graphene semiconductors.`, 
-        delay: 1, 
-        targetID: "about-line-2", 
-        removeCursor: true},
-        { text: "In short: I enjoy building things that closely interact with our world, and this is only the beginning.",
-        delay: 1,
-        targetID: "about-line-3",
-        removeCursor: true}
-    ],
-    "hobbies-section": [
-        { text: "Hobbies", delay: 1, targetID: "hobbies-head", removeCursor: true },
-        { text: "Engineering can be exhausting, so I like to wind down in the following ways:", delay: 1, targetID: "hobbies-text-span", removeCursor: true},
-        { text: ""}
-    ]
+// const sections = {
+//     "about-section": [
+//         { text: "./about-me", delay: 50, targetID: "about-command", removeCursor: true},
+//         { text: "Background", delay: 1, targetID: "about-back-head", removeCursor: true},
+//         { text: `Hey there—My name is Cordell Palmer. In December 2024, I graduated from Georgia Tech 
+//             with a Bachelor of Science in Mechanical Engineering and a Minor in Computer Science, with a focus on Machine Learning.
+//             During my undergraduate years, I was able to cultivate a broad set of skills, having obtained proficiency in areas such as
+//             CAD modeling/3D printing, structural analysis, software and hardware programming, AI, robotics, and wide range of electrical and mechanical
+//             data analysis/manufacturing tools. Despite my background being primarily rooted in mechanical systems, my true passion lies at the intersection of hardware, 
+//             software, and AI development.`, 
+//         delay: 1, 
+//         targetID: "about-line-1", 
+//         removeCursor: true},
+//         { text: `Since graduation I've deeply immersed myself in systems development, having written the HTTP server that
+//             runs this website from scratch in C++ with barebones kernel APIs. The journey to enrich my understanding of networking, memory, and 
+//             OS-level architecture has sparked an ever evolving passion in all things embedded systems, FPGAs, digital circuitry, and future-forward
+//             technologies like graphene semiconductors.`, 
+//         delay: 1, 
+//         targetID: "about-line-2", 
+//         removeCursor: true},
+//         { text: "In short: I enjoy building things that closely interact with our world, and this is only the beginning.",
+//         delay: 1,
+//         targetID: "about-line-3",
+//         removeCursor: true}
+//     ],
+//     "hobbies-section": [
+//         { text: "Hobbies", delay: 1, targetID: "hobbies-head", removeCursor: true },
+//         { text: "Engineering can be exhausting, so I like to wind down in the following ways:", delay: 1, targetID: "hobbies-text-span", removeCursor: true},
+//         { text: ""}
+//     ]
+// }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
